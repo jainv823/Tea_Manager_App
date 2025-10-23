e861651c88b86536c5453dc7a49ec63ceebbbc8a
Vinay jain <jainv823@gmail.com>
2025-10-23 13:44:40 +0530

    Advance logging with morgan and winston

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 import express from "express";
+import "dotenv/config";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
+const morganFormat = ":method :url :status :response-time ms";
 
 app.use(express.json());
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = [];
 let nextId = 1;
